53d66b1e5cb79c5d1ce85201f20c432e7c03ed79
mraethel <mraethel@outlook.com>
2020-03-19 22:48:43 +0100

    Test: Update;

--- references.go
+++ references.go
@@ -1,244 +1,121 @@
 package main
 
 import(
-	"encoding/json"
-	"io/ioutil"
-	"crypto/sha256"
-	"os"
 	"fmt"
+	"io/ioutil"
+	"encoding/json"
+	"os"
 	"path"
+	"crypto/sha256"
 	"strings"
+	"github.com/nsgtest/packages/interfaces"
+	"github.com/nsgtest/packages/structs"
 )
+
+var messages []string = []string{
+	"No similar reference found!",
+	"Reference name already assigned:",
+	"Something went horribly wrong:",
+	"Found same reference with differenct content(SHA256):",
+	"Found reference with same content(SHA256):",
+	"Found same reference with different URL:",
+	"Something went horribly wrong.",
+	"Found identical reference:"}
 
 type Reference struct{
 	Name, Url	string
 	Checksum	[32]byte
 }
 
-func (reference Reference) list(){
-	fmt.Printf("Name:\t\t%v\nUrl:\t\t%v\nChecksum:\t%v\n", reference.Name, reference.Url, strings.Trim(fmt.Sprintf("%v", reference.Checksum), "[]"))
-}
-
 type References []Reference
-
-func (references References) write(file string){
-	_, err := os.Stat(path.Dir(file))
-	if os.IsNotExist(err){
-		fmt.Printf("FAIL!\nDirectory %v does not exist!\n\n", path.Dir(file))
-		panic(err)
-	}
-
-	data, _ := json.Marshal(references)
-
-	err = ioutil.WriteFile(file, data, 0666)
-	if err != nil{
-		fmt.Printf("FAIL!\nCould not write to %v\n\n", file)
-		panic(err)
-	}
-}
-
-func (references References) find(file string) int{
-	for i, reference := range references{
-		if reference.Name == path.Base(file){
-			return i
-		}
-	}
-
-	fmt.Printf("FAIL!\nNo reference %v in references!\n\n", path.Base(file))
-	panic(nil)
-}
-
-func (references References) validate(file string, codes []int) Reference{
-	comparison := Reference{
-		Name:		path.Base(file),
-		Url:		url(file),
-		Checksum:	checksum(file),
-	}
-
-	black := false
-	zero := true
-
-	messages:= []string{
-		"No similar reference found!",
-		"Found reference with same content(SHA256)!",
-		"Something went horribly wrong!",
-		"Something went horribly wrong!",
-		"Reference name already assigned!",
-		"Found same reference with different URL!",
-		"Found same reference with differenct content(SHA256)!",
-		"Found identical reference!"}
-
-	for _, reference := range references{
-		value := 0
-
-		if reference.Name == comparison.Name{
-			value += 4
-		}
-		if reference.Url == comparison.Url{
-			value += 2
-		}
-		if reference.Checksum == comparison.Checksum{
-			value += 1
-		}
-
-		for i, code := range codes{
-			if value == code{
-				zero = false
-				break
-			}
-
-			if i == len(codes)-1 && value > 0{
-				fmt.Printf("FAIL!\n" + messages[value] + "\nCurrent:\n\n")
-				comparison.list()
-				fmt.Printf("\nSimilar:\n\n")
-				reference.list()
-				fmt.Printf("\n")
-
-				black = true
-			}
-		}
-	}
-
-	if black{
-		panic(nil)
-	}
-	if len(references) > 0 && zero{
-		fmt.Printf("FAIL!\n" + messages[0] + "\n\n")
-		panic(nil)
-	}
-
-	return comparison
-}
 
 func main(){
 	if len(os.Args) > 1{
 		switch os.Args[1]{
-		case "new":
+		case "init":
 			if len(os.Args) == 3{
-				newreferences(os.Args[2])
+				s := structs.Struct{os.Args[2], Reference{}, References{}.ota()}
+				s.Write()
 			} else {
 				help()
 			}
 		case "add":
 			if len(os.Args) == 4{
-				addreference(os.Args[2], os.Args[3])
-			} else {
-				help()
-			}
-		case "remove":
-			if len(os.Args) == 4{
-				removereference(os.Args[2], os.Args[3])
+				s := structs.Struct{os.Args[2], Reference{path.Base(os.Args[3]), url(os.Args[3]), checksum(os.Args[3])}, read(os.Args[2]).ota()}
+				s.Add([]int{1,2,3,4,5,6,7})
 			} else {
 				help()
 			}
 		case "update":
 			if len(os.Args) == 4{
-				updatereference(os.Args[2], os.Args[3])
+				s := structs.Struct{os.Args[2], Reference{path.Base(os.Args[3]), url(os.Args[3]), checksum(os.Args[3])}, read(os.Args[2]).ota()}
+				s.Update([]int{3,5})
+			} else {
+				help()
+			}
+		case "remove":
+			if len(os.Args) == 4{
+				s := structs.Struct{os.Args[2], Reference{os.Args[3], "", [32]byte{}}, read(os.Args[2]).ota()}
+				s.Remove([]int{1})
 			} else {
 				help()
 			}
 		case "list":
 			if len(os.Args) == 3{
-				listreferences(os.Args[2])
+				s := structs.Struct{os.Args[2], Reference{}, read(os.Args[2]).ota()}
+				s.List()
 			} else {
 				help()
 			}
 		default:
 			help()
 		}
-	} else{
+	} else {
 		help()
 	}
 }
 
-func newreferences(name string){
-	fmt.Printf("Creating new references... ")
-
-	var references References
-	references.write(name)
-
-	fmt.Printf("Done!\n")
+func (r Reference) Message(count int){
+	fmt.Printf(messages[count])
 }
 
-func addreference(name, file string){
-	fmt.Printf("Adding reference to references... ")
-
-	references := read(name)
-
-	reference := references.validate(file, []int{0})
-
-	references = append(references, reference)
-
-	references.write(name)
-
-	fmt.Printf("Done!\n")
-}
-
-func removereference(name, reference string){
-	fmt.Printf("Removing reference from references... ")
-
-	references := read(name);
-
-	i := references.find(reference)
-
-	references[i]=references[len(references)-1]
-
-	references[:len(references)-1].write(name)
-
-	fmt.Printf("Done!\n")
-}
-
-func updatereference(name, file string){
-	fmt.Printf("Updating reference in references... ")
-
-	references := read(name)
-
-	reference := references.validate(file, []int{4, 5, 6})
-
-	i := references.find(file)
-
-	references[i] = reference
-
-	references.write(name)
-
-	fmt.Printf("Done!\n")
-}
-
-func listreferences(name string){
-	references := read(name)
-
-	if len(references) < 1{
-		fmt.Printf("No reference found in references %v!\n", name)
-		panic(nil)
+func (r References) ota() interfaces.Interfaces{
+	array := make(interfaces.Interfaces, len(r))
+	for i, reference := range r{
+		array[i] = reference
 	}
-
-	references[0].list()
-
-	for _, reference := range references[1:]{
-		fmt.Printf("\n")
-		reference.list()
-	}
+	return array
 }
 
-func help(){
-	fmt.Printf("Usage: ref COMMAND [ARGUMENT]\nCreate references and add, remove or modify a reference.\n\nCOMMANDS:\n\tnew NAME\t\tCreate new refernces named NAME.\n\tadd NAME FILE\t\tAdd reference FILE to references NAME.\n\tremove NAME REFERENCE\tRemove REFERENCE from reference NAME.\n\tupdate NAME FILE\tUpdate reference FILE in reference NAME.\n\tlist NAME\t\tList references NAME.\n\nARGUMENTS:\n\tNAME\t\tValid JSON file name.\n\tFILE\t\tAbsolute Path to file.\n\tREFERENCE\tName of a reference in references.\n")
-}
-
-func read(file string) References{
+func checksum(file string) [32]byte{
 	data, err := ioutil.ReadFile(file)
 	if err != nil{
 		fmt.Printf("FAIL!\nCould not read from %v!\n\n", file)
 		panic(err)
 	}
 
-	var references References
-	err = json.Unmarshal(data, &references)
+	return sha256.Sum256(data)
+}
+
+func help(){
+	fmt.Printf("Usage: refs COMMAND [ARGUMENT]\nCreate references and add, modify or remove a reference.\n\nCOMMANDS:\n\tnew NAME\t\tCreate new refernces named NAME.\n\tadd NAME FILE\t\tAdd reference FILE to references NAME.\n\tupdate NAME FILE\tUpdate reference FILE in reference NAME.\n\tremove NAME REFERENCE\tRemove REFERENCE from reference NAME.\n\tlist NAME\t\tList references NAME.\n\nARGUMENTS:\n\tNAME\t\tValid JSON file name.\n\tFILE\t\tAbsolute Path to file.\n\tREFERENCE\tName of a reference in references.\n")
+}
+
+func read(file string) References{
+	enc, err := ioutil.ReadFile(file)
+	if err != nil{
+		fmt.Printf("FAIL!\nCould not read from %v!\n\n", file)
+		panic(err)
+	}
+
+	r := References{}
+	err = json.Unmarshal(enc, &r)
 	if err != nil{
 		fmt.Printf("FAIL!\n%v is not a JSON file!\n\n", file)
 		panic(err)
 	}
 
-	return references
+	return r
 }
 
 func url(file string) string{
@@ -275,21 +152,11 @@ func url(file string) string{
 	}
 
 	split := strings.Split(string(data), " ")
-	url := split[len(split) - 1]
+	url := strings.TrimSuffix(split[len(split) - 1], "\n")
 
 	repository := strings.TrimSuffix(path.Base(url), path.Ext(url))
 	url = path.Dir(url)
 	repository = path.Join(path.Base(url), repository)
 
 	return path.Join("https://api.github.com/repos", repository, "contents", content + "?ref=master")
-}
-
-func checksum(file string) [32]byte{
-	data, err := ioutil.ReadFile(file)
-	if err != nil{
-		fmt.Printf("FAIL!\nCould not read from %v!\n\n", file)
-		panic(err)
-	}
-
-	return sha256.Sum256(data)
 }
